35bd9c22307e3f474b90ea78da117dc5a7d3b985
Brendan Kirby <bkirby@bntky.com>
2018-03-11 18:21:18 -0700

    Step 6 - Added a test for create page and made it pass

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -9,4 +9,13 @@ describe('User visits root', () => {
       assert.equal(browser.getText('#videos-container'), '');
     });
   });
+  describe('and can navigate', () => {
+    it('to videos/create', () => {
+      browser.url('/');
+
+      browser.click('a[href="videos/create.html"]');
+
+      assert.include(browser.getText('body'), 'Save a video');
+    });
+  });
 });
