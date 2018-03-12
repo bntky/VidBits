2b5fcab7c6e5a6d02445d5977db9c5615683d3b8
Brendan Kirby <bkirby@bntky.com>
2018-03-11 19:54:12 -0700

    Step 11 -- Add title field to Video model with a test to see that it's a string
    Test is passing.  Also, added test-utils so that colors look correct.

--- test/models/video-test.js
+++ test/models/video-test.js
@@ -1,6 +1,10 @@
 const {assert} = require('chai');
 const {mongoose, databaseUrl, options} = require('../../database');
 
+require('../test-utils');
+
+const Video = require('../../models/video.js');
+
 async function connectDatabase() {
   await mongoose.connect(databaseUrl, options);
   await mongoose.connection.db.dropDatabase();
@@ -9,6 +13,22 @@ async function connectDatabase() {
 async function disconnectDatabase() {
   await mongoose.disconnect();
 }
+
+describe('Model: Video', () => {
+  beforeEach(connectDatabase);
+
+  afterEach(disconnectDatabase);
+
+  describe('title field', () => {
+    it('is a string', async () => {
+      const title = 314159;
+
+      const video = new Video({ title });
+
+      assert.strictEqual(video.title, title.toString());
+    });
+  });
+});
 
 module.exports = {
   connectDatabase,
