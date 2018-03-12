71938303ba9ecd478b7c4a5386dbd47cb74a22d6
Brendan Kirby <bkirby@bntky.com>
2018-03-11 18:04:13 -0700

    Add code to make test colors more visible

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,4 @@
 const {assert} = require('chai');
+
+require('../test-utils');
+
---
+++ test/test-utils.js
@@ -0,0 +1,6 @@
+var colors = require('mocha/lib/reporters/base').colors;
+colors['pass'] = '97';
+colors['fast'] = '97';
+colors['light'] = '97';
+colors['diff gutter'] = '97';
+colors['error stack'] = '97';
