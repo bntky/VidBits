04b036307c6777f035c4c5c03d54b5d3c78eacc2
Brendan Kirby <bkirby@bntky.com>
2018-03-11 18:04:59 -0700

    Step 3 - Write first feature test and watch it fail

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -2,3 +2,11 @@ const {assert} = require('chai');
 
 require('../test-utils');
 
+describe('User visits root', () => {
+  describe('without any videos', () => {
+    it('starts blank', () => {
+      browser.url('/');
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+});
