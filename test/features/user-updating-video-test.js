1401295064b0744ea4c17a0e63a5553b7c0cf7c4
Brendan Kirby <bkirby@bntky.com>
2018-03-17 12:45:48 -0700

    Step 38 -- Start creating a new edit page add link to show page
    Adds a test to click on the edit link in the show page and verify that
    the title can be changed.  Requires going to server level to fix.

---
+++ test/features/user-updating-video-test.js
@@ -0,0 +1,25 @@
+const {assert} = require('chai');
+
+require('../test-utils');
+
+describe('User updating a video', () => {
+  describe('changes the values', () => {
+    it('of the title and sees new title rendered', () => {
+      const title = "My first title";
+      const description = "A long description of some interesting train video";
+      const url = 'https://www.youtube.com/watch?v=3EGOwfWok5s';
+      const newTitle = 'Change the title to something else';
+
+      browser.url('/videos/create.html');
+      browser.setValue('#title-input', title);
+      browser.setValue('#description-input', description);
+      browser.setValue('#url-input', url);
+      browser.click('#submit-button');
+      browser.click('#edit');
+      browser.setValue('#title-input', newTitle);
+      browser.click('#submit-button');
+
+      assert.include(browser.getText('body'), newTitle);
+    });
+  });
+});
