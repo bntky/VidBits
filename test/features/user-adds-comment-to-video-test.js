8d1fe193ebaa5c340537c7f60a89b0b5c69e5573
Brendan Kirby <bkirby@bntky.com>
2018-03-25 16:15:00 -0700

    Start adding comments feature to videos

---
+++ test/features/user-adds-comment-to-video-test.js
@@ -0,0 +1,30 @@
+const {assert} = require('chai');
+
+require('../test-utils');
+
+const generateRandomUrl = (domain) => {
+  return `http://${domain}/${Math.random()}`;
+};
+
+const addNewVideo = (title, description, url) => {
+  browser.url('/videos/create.html');
+  browser.setValue('#title-input', title);
+  browser.setValue('#description-input', description);
+  browser.setValue('#url-input', url);
+  browser.click('#submit-button');
+  browser.url('/');
+};
+
+describe('User comments', () => {
+  describe('on an existing video', () => {
+    it('starts blank', () => {
+      const title = 'A Train vidoe';
+      const description = 'Train drives down train tracks.  Honks horn.';
+      const url = generateRandomUrl('www.youtube.com');
+      addNewVideo(title, description, url);
+      browser.click('.show-video');
+
+      assert.equal(browser.getText('#comments-container'), '');
+    });
+  });
+});
